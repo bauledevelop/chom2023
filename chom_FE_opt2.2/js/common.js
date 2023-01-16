2078afe2e9a26c2e08fcf1e647eae581c53a5fbf
Nguyễn Tuấn Phong <pn02102001@gmail.com>
2023-01-16 19:29:25 +0700

    fix page contact2

--- chom_FE_opt2.2/js/common.js
+++ chom_FE_opt2.2/js/common.js
@@ -1909,23 +1909,23 @@ function Shortcodes() {
 
   // Accordion
 
-  $("dd.accordion-content").slideUp(1).addClass("hide");
-  $("dl.accordion").on("click", "dt", function () {
-    $(this)
-      .addClass("accordion-active")
-      .next()
-      .slideDown(350)
-      .siblings("dd.accordion-content")
-      .slideUp(350)
-      .prev()
-      .removeClass("accordion-active");
-  });
-  $("dl.accordion").on("click", "dt.accordion-active", function () {
-    $(this)
-      .removeClass("accordion-active")
-      .siblings("dd.accordion-content")
-      .slideUp(350);
-  });
+  // $("dd.accordion-content").slideUp(1).addClass("hide");
+  // $("dl.accordion").on("click", "dt", function () {
+  //   $(this)
+  //     .addClass("accordion-active")
+  //     .next()
+  //     .slideDown(350)
+  //     .siblings("dd.accordion-content")
+  //     .slideUp(350)
+  //     .prev()
+  //     .removeClass("accordion-active");
+  // });
+  // $("dl.accordion").on("click", "dt.accordion-active", function () {
+  //   $(this)
+  //     .removeClass("accordion-active")
+  //     .siblings("dd.accordion-content")
+  //     .slideUp(350);
+  // });
 
   $(".flexnav").flexNav({ animationSpeed: 250 });
 
