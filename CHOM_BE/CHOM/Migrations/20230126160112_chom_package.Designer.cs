18344f8d5680b6c77e63c645619d66771620cd63
Nguyễn Tuấn Phong <pn02102001@gmail.com>
2023-01-27 19:34:56 +0700

    fix page gallery

--- CHOM_BE/CHOM/Migrations/20230126160112_chom_package.Designer.cs
+++ CHOM_BE/CHOM/Migrations/20230126160112_chom_package.Designer.cs
@@ -4,6 +4,7 @@ using CHOM.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CHOM.Migrations
 {
     [DbContext(typeof(CHOMContext))]
-    partial class CHOMContextModelSnapshot : ModelSnapshot
+    [Migration("20230126160112_chom_package")]
+    partial class chom_package
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CHOM_BE/CHOM/Migrations/20230126160112_chom_package.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CHOM.Migrations
+{
+    public partial class chom_package : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
